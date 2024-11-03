62cf87d73e7fcaf8d9b87d10b40162c9e8aa5b44
annagorsh <annagorshhh@gmail.com>
2024-11-04 01:38:07 +0300

    написаны тесты на регистрацию и логин

---
+++ links.py
@@ -0,0 +1,7 @@
+MAIN_URL = "https://stellarburgers.nomoreparties.site/api/"
+
+CREATE_USER_URL = MAIN_URL+"auth/register"
+
+DELETE_USER_URL = MAIN_URL+"auth/user"
+
+LOGIN_URL = MAIN_URL+"auth/login"
---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import string
+import pytest
+import random
+from random import randint
+
+
+@pytest.fixture
+def payload():
+    def generate_random_string(length):
+        letters = string.ascii_lowercase
+        random_string = ''.join(random.choice(letters) for i in range(length))
+        return random_string
+
+    email = str(randint(10000, 99999)) + "@gmail.com"
+    password = str(randint(1000000, 9999999))
+    name = generate_random_string(10)
+
+    payload = {
+        "email": email,
+        "password": password,
+        "name": name
+    }
+
+    return payload
+
+@pytest.fixture
+def incomplete_user_data():
+    email = str(randint(10000, 99999)) + "@gmail.com"
+    password = str(randint(1000000, 9999999))
+    incomplete_user_data = {
+        "email": email,
+        "password": password
+    }
+    return incomplete_user_data
